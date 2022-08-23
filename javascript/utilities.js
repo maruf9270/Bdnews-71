6406d5b2cd0c403ad20b9acd38fecaf77d86e2c5
Maruf9270 <maruf9270@gmail.com>
2022-08-23 18:28:43 +0600

    added swicth For inserting player and disabling button

--- javascript/utilities.js
+++ javascript/utilities.js
@@ -1,3 +1,5 @@
+// Insert player function
+
 function insertPlayer(playerName){
     const ul = document.getElementById('playerList');
 
@@ -7,6 +9,12 @@ function insertPlayer(playerName){
     ul.appendChild(li);
 }
 
+
+// Disable button function
+function disableButton(buttonName){
+    const clicked = document.getElementById(buttonName);
+    clicked.setAttribute('disabled', true)
+}
 document.getElementById("palyer-section").addEventListener('click', function(event){
     // console.log(event.target.id)
     const click = event.target.id;
@@ -15,62 +23,116 @@ document.getElementById("palyer-section").addEventListener('click', function(eve
     const total = document.querySelectorAll('#playerList li')
     // console.log(total.length)
     if(total.length === 5){
-        alert("onstfkakj")
+        alert("Youv'e selectd maximum Player.")
     }
     else{
         
+        switch(click){
+            case 'messi':
+                insertPlayer('Leonel Messi');
+                disableButton('messi');
+                break;
+            
+            case 'kylina':
+                insertPlayer('Kylian Mbappé');
+                disableButton('kylina');
+                break;
+            
+            case 'naymer':
+                insertPlayer('Neymar Jr');
+                disableButton('naymer');
+                break;
 
-        if(click === 'messi'){
-            insertPlayer('Leonel Messi');
-            const clicked = document.getElementById('messi');
-            clicked.setAttribute('disabled', true)
-        }
-        if(click === 'kylina'){
-            insertPlayer('Mbappe');
-            const clicked = document.getElementById('kylina');
-            clicked.setAttribute('disabled', true)
-        }
+            case 'vitor':
+                insertPlayer('Vítor Machado');
+                disableButton('vitor');
+                break;
+            
+            case 'ramos':
+                insertPlayer('Sergio Ramos');
+                disableButton('ramos');
+                break;
 
-        if(click === 'naymer'){
-            insertPlayer('Naymer');
-            const clicked = document.getElementById('naymer');
-            clicked.setAttribute('disabled', true)
-        }
+            case 'renato':
+                insertPlayer('Renato Sanches');
+                disableButton('renato');
+                break;
 
-        if(click === 'vitor'){
-            insertPlayer('Vítor Machado');
-            const clicked = document.getElementById('vitor');
-            clicked.setAttribute('disabled', true)
-        }
+            case 'ronaldino':
+                insertPlayer('Ronaldinho');
+                disableButton('ronaldino');
+                break;
 
-        if(click === 'ramos'){
-            insertPlayer('Sergio Ramos');
-            const clicked = document.getElementById('ramos');
-            clicked.setAttribute('disabled', true)
-        }
+            case 'ronaldo':
+                insertPlayer('Cristiano Ronaldo');
+                disableButton('ronaldo');
+                break;
 
-        if(click === 'renato'){
-            insertPlayer('Renato Sanches');
-            const clicked = document.getElementById('renato');
-            clicked.setAttribute('disabled', true)
-        }
+            case 'romero':
+                insertPlayer('Sergio Romero');
+                disableButton('romero');
+                break;
+             }
+
         // if(click === 'messi'){
         //     insertPlayer('Leonel Messi');
-        //     const clicked = document.getElementById('messi');
-        //     clicked.setAttribute('disabled', true)
+        //     // const clicked = document.getElementById('messi');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('messi');
         // }
-        // if(click === 'messi'){
-        //     insertPlayer('Leonel Messi');
-        //     const clicked = document.getElementById('messi');
-        //     clicked.setAttribute('disabled', true)
+        // if(click === 'kylina'){
+        //     insertPlayer('Mbappe');
+        //     // const clicked = document.getElementById('kylina');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('kylina');
         // }
-        // if(click === 'messi'){
-        //     insertPlayer('Leonel Messi');
-        //     const clicked = document.getElementById('messi');
-        //     clicked.setAttribute('disabled', true)
+
+        // if(click === 'naymer'){
+        //     insertPlayer('Naymer');
+        //     // const clicked = document.getElementById('naymer');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('naymer');
         // }
-        // const total = document.querySelectorAll('#playerList li')
-        // console.log(total.length)
+
+        // if(click === 'vitor'){
+        //     insertPlayer('Vítor Machado');
+        //     // const clicked = document.getElementById('vitor');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('vitor');
+        // }
+
+        // if(click === 'ramos'){
+        //     insertPlayer('Sergio Ramos');
+        //     // const clicked = document.getElementById('ramos');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('ramos');
+        // }
+
+        // if(click === 'renato'){
+        //     insertPlayer('Renato Sanches');
+        //     // const clicked = document.getElementById('renato');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('renato');
+        // }
+        // if(click === 'ronaldino'){
+        //     insertPlayer('Ronaldinho');
+        //     // const clicked = document.getElementById('renato');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('ronaldino');
+        // }
+        // if(click === 'ronaldo'){
+        //     insertPlayer('Cristiano Ronaldo');
+        //     // const clicked = document.getElementById('renato');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('ronaldo');
+        // }
+        // if(click === 'romero'){
+        //     insertPlayer('Sergio Romero');
+        //     // const clicked = document.getElementById('renato');
+        //     // clicked.setAttribute('disabled', true)
+        //     disableButton('romero');
+        // }
+        
 
     }
 
@@ -94,10 +156,12 @@ document.getElementById('calculate').addEventListener('click', function(){
 //  console.log(playerCost);
 // //  console.log('ppppp')
     const perPlayerCost = document.getElementById('perplayercost').value;
+    
     const totalPlayer = document.querySelectorAll('#playerList li').length;
 
 
     const perPlayerCostFloat = parseFloat(perPlayerCost);
+    console.log(isNaN(perPlayerCostFloat));
 
     const playerCost = perPlayerCostFloat*totalPlayer;
 
